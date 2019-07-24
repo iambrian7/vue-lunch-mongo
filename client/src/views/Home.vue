e74d20bb9956b3aab3cd8d20e80d8a7757a115c4
iambrian7 <bkcgee@gmail.com>
2019-07-24 18:24:54 -0500

    test ssh key

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -59,7 +59,7 @@ export default {
     }
   },
   created() {
-    console.log("created home.....really home.... loadingGroups")
+    console.log("created home.....really home.... loading....")
     this.loadGroups()
   }
 }
