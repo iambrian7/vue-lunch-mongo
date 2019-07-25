c89c423030b3ff45cb9955de92f9639e95176237
iambrian7 <bkcgee@gmail.com>
2019-07-25 08:34:46 -0500

    more testing 2

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -59,7 +59,7 @@ export default {
     }
   },
   created() {
-    console.log("created home.....really home.xxx loading....")
+    console.log("created home..really home.xxx loading....")
     this.loadGroups()
   }
 }
